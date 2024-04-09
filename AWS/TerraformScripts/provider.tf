a87e7029d1c5a97e94c497f893443a56ee985361
alelahnisbett <anisbett97@gmail.com>
2024-04-09 13:39:43 +0200

    provider changes

--- AWS/TerraformScripts/provider.tf
+++ AWS/TerraformScripts/provider.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 2.0"
+      version = "5.24.0"
     }
   }
 }
@@ -12,6 +12,6 @@ terraform {
 # Configure the AWS Provider
 
 provider "aws" {
-    # profile = "default"
+    profile = "default"
     region = "eu-central-1"
 }
